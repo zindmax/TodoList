7b42168e9736f5896a5de528cc4008e9b727b7bb
zindmax <s1ndbad1337@gmail.com>
2022-03-16 21:33:42 +0800

    add user auth

--- app/Http/Controllers/TodoItemController.php
+++ app/Http/Controllers/TodoItemController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
     public function index() {
-        $todos = Todo::get();
+        $todos = User::find(Auth::id())->todos;
         return view("todo", ["todos" => $todos]);
     }
 
     public function store(Request $request) {
         $todo = new Todo();
         $todo->todo = $request->input("todo_add");
+        $todo->user_id = Auth::id();
         $todo->save();
         return redirect("/todo");
     }
